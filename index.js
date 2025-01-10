d47eae14bdd02c60784affe60d76693cc00dd08b
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2025-01-10 20:29:49 +0200

    implement separate html blocks for each page

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Serve the index.html file when visiting the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    const mainPagePath = path.join(__dirname, 'public', 'index.html');
+    const page1Path = path.join(__dirname, 'public', 'pages', 'page-01.html');
+    const page2Path = path.join(__dirname, 'public', 'pages', 'page-02.html');
+    const page3Path = path.join(__dirname, 'public', 'pages', 'page-03.html');
+    const mainPage = fs.readFileSync(mainPagePath, 'utf-8');
+    const page1 = fs.readFileSync(page1Path, 'utf-8');
+    const page2 = fs.readFileSync(page2Path, 'utf-8');
+    const page3 = fs.readFileSync(page3Path, 'utf-8');
+    const modifiedPage = mainPage
+        .replace('<!-- page-01-content -->', page1)
+        .replace('<!-- page-02-content -->', page2)
+        .replace('<!-- page-03-content -->', page3);
+    res.send(modifiedPage);
 });
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
 app.listen(port, () => {
